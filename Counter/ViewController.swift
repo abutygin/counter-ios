9e14b81a4077cfe21233214506b65806897bcd48
Aleksandr Butygin <a.butygin@gmail.com>
2025-11-19 23:32:00 +0300

    sprint_03_complicated. Counter with +, -, reset buttons and log textView

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,25 +10,76 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var incrementButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var logTextView: UITextView!
     private var counterNumber: Int = 0
+    private var logList: [String] = ["История изменений:"]
     var counterText: String {
         "Значение счётчика: \(counterNumber)"
+    }
+    
+    var logText: String {
+        logList.joined(separator: "\n")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        counterLabel.text = getCounterText()
+        counterLabel.text = counterText
+        logTextView.isEditable = false
+        updateLogTextView()
     }
 
-    func getCounterText() -> String {
-        return "Значение счётчика: \(counterNumber)"
-    }
-
-    @IBAction func incrementButtonDidTap() {
+    @IBAction func plusButtonDidTap() {
         counterNumber += 1
         counterLabel.text = counterText
+        logText("значение изменено на +1")
     }
+    
+    @IBAction func minusButtonDidTap() {
+        if counterNumber > 0 {
+            counterNumber -= 1
+            counterLabel.text = counterText
+            logText("значение изменено на -1")
+        } else {
+            logText("попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    @IBAction func resetButtonDidTap() {
+        if counterNumber == 0 {
+            return
+        }
+        counterNumber = 0
+        counterLabel.text = counterText
+        logText("значение сброшено")
+    }
+    
+    func logText(_ text: String) {
+        let fullRecord = currentFormattedDateAndTime() + ": " + text
+        logList.append(fullRecord)
+        updateLogTextView()
+    }
+    
+    func updateLogTextView() {
+        logTextView.text = logText
+        logTextView.layoutManager.allowsNonContiguousLayout = false
+        let bottom = NSMakeRange(logTextView.text.count - 1, 1)
+        logTextView.scrollRangeToVisible(bottom)
+    }
+}
+
+private extension ViewController {
+
+    func currentFormattedDateAndTime() -> String {
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateString = dateFormatter.string(from: currentDate)
+        return dateString
+    }
+
 }
 
